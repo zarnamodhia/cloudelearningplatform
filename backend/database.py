b64cdad1eb443d584f78f1ef9428c7205d82aca4
Zarna <modhiazarnaa@gmail.com>
2026-04-03 23:12:52 +0530

    updtedd

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./elearning.db"
+DATABASE_URL = "postgresql://elearing_user:fJc7TsG06EnPBMXRIJAsg3Dclmy2iZTu@dpg-d77up09r0fns738bb4l0-a.oregon-postgres.render.com/elearning_vkyp"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
