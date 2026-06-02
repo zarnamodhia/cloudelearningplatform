b23a09cb25a4f588a952f6833f2d024086bb9e7a
Zarna Modhia <158729383+zarnamodhia@users.noreply.github.com>
2026-06-02 10:35:51 +0530

    Update DATABASE_URL to use environment variable

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("postgresql://elearning_l2qi_user:Qe0NkTQf57DB5668dkqGiT47LNFjsrHh@dpg-d82o5rgg4nts73b6ta20-a.singapore-postgres.render.com/elearning_l2qi")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine)
