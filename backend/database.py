ef240b49878bba0801024de143b41318bb88bc7f
Zarna Modhia <158729383+zarnamodhia@users.noreply.github.com>
2026-06-02 10:26:15 +0530

    Update DATABASE_URL for database connection

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("postgresql://elearing_user:fJc7TsG06EnPBMXRIJAsg3Dclmy2iZTu@dpg-d77up09r0fns738bb4l0-a.oregon-postgres.render.com/elearning_vkyp")
+DATABASE_URL = os.getenv("postgresql://elearning_l2qi_user:Qe0NkTQf57DB5668dkqGiT47LNFjsrHh@dpg-d82o5rgg4nts73b6ta20-a.singapore-postgres.render.com/elearning_l2qi")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine)
